e236d9d100c5c26fae2255f85da8aacf6b6a283e
behzad <121344556+behzadomidi@users.noreply.github.com>
2023-04-24 09:28:06 +0330

    final

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Reviews from "./Reviews";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <div className="title">
+          <h2>نظرات همکاران</h2>
+        </div>
+        <Reviews />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const reviews = [
+  {
+    id: 1,
+    name: " مهسا امینی ",
+    job: "طراح وب",
+    image:
+      "https://www.cutislaserclinics.com/wp-content/uploads/2018/02/Achieve-Youthful-V-Shape-Face.jpg",
+    text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است",
+  },
+  {
+    id: 2,
+    name: "شیوا شهینی",
+    job: "گرافیست",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFtoF8Xim-1NJ-QG8YS1_pumJth5gN-NX_fw&usqp=CAU",
+    text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و  است",
+  },
+  {
+    id: 3,
+    name: "شهین شایانی",
+    job: "برنامه نویس",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGFKO8juT-tvC5yTj6DB7LF9JIQyrYBWGUKcZ1vxGO7uA2ybkws1JpaYGBXctrDjKi_zw&usqp=CAU",
+    text: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ت",
+  },
+];
+
+export default reviews;
